b7aae4cda97fe678f3b0c809d91f8344b697ffae
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-04 00:02:40 +0900

    style header + navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,13 +3,32 @@ import './Navigation.scss';
 
 import DrawerToggleButton from './DrawerToggleButton';
 
+import JNTImage from '../assets/images/jnt.png';
+
 class Navigation extends Component {
 
   render() {
     return (
       <div className='container-fluid navigation-wrapper'>
-        <DrawerToggleButton click={this.props.handleDrawerToggleClick} />
-        This is navigation
+        <ul className='nav-left d-md-none'>
+          <li>
+            <DrawerToggleButton click={this.props.handleDrawerToggleClick} />
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <img src={JNTImage} alt="Personal Logo" style={{ height: '32px' }}/>
+          </li>
+        </ul>
+        <ul className='nav-right d-none d-md-flex'>
+          <li>some text</li>
+          <li>This is navigation</li>
+        </ul>
+        <ul className='nav-right d-md-none'>
+          <li>
+            <div style={{ height: '20px', width: '18px'}}></div>
+          </li>
+        </ul>
       </div>
     );
   }
